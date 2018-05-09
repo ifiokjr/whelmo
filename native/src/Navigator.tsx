193cc9c05c643f2da72172a1308bc5e2f30baa5e
Ifiok Jr <ifiokotung@gmail.com>
2018-05-09 15:20:47 +0100

    WIP: update deps

--- native/src/Navigator.tsx
+++ native/src/Navigator.tsx
@@ -2,10 +2,12 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/Feather';
 import {
+  DrawerNavigator,
   HeaderMode,
   NavigationTabScreenOptions,
   StackNavigator,
   SwitchNavigator,
+  TabBarBottom,
   TabNavigator,
 } from 'react-navigation';
 import { screenRoutes, stackRoutes } from './constants';
@@ -16,6 +18,7 @@ import {
   ShareScreen,
   SignInScreen,
 } from './screens';
+// import { Fab } from 'native-base';
 
 // Implementation of HomeScreen, OtherScreen, SignInScreen, AuthLoadingScreen
 // goes here.
@@ -73,6 +76,7 @@ const HomeTabs = TabNavigator(
     },
   },
   {
+    tabBarComponent: TabBarBottom,
     animationEnabled: true,
     tabBarPosition: 'bottom',
     swipeEnabled: false,
@@ -82,10 +86,36 @@ const HomeTabs = TabNavigator(
   },
 );
 
+// const HomeTabsWithFab: React.StatelessComponent<{}> = ({
+//   children,
+//   ...props
+// }) => (
+//   <HomeTabs {...props}>
+//     <Fab
+//       direction="up"
+//       containerStyle={{}}
+//       style={{ backgroundColor: Theme.palette.primary }}
+//       position="bottomRight"
+//       onPress={this.onPressNewNote}
+//     >
+//       <Icon name="plus" size={30} />
+//     </Fab>
+//   </HomeTabs>
+// );
+
+const HomeDrawer = DrawerNavigator(
+  {
+    HomeDrawer: { screen: HomeTabs },
+  },
+  {
+    initialRouteName: 'HomeDrawer',
+  },
+);
+
 export default SwitchNavigator(
   {
     [screenRoutes.AppLoading]: AppLoadingScreen,
-    [screenRoutes.Tabs]: HomeTabs,
+    [screenRoutes.Tabs]: HomeDrawer,
     [stackRoutes.Auth]: AuthStack,
   },
   {
--- native/src/screens/HomeScreen.tsx
+++ native/src/screens/HomeScreen.tsx
@@ -1,8 +1,12 @@
 import { inject } from 'mobx-react/native';
+import { Fab, Icon } from 'native-base';
 import React, { Component } from 'react';
 import { Button, StyleSheet, View } from 'react-native';
+// import Icon from 'react-native-vector-icons/Feather';
 import { NavigationInjectedProps } from 'react-navigation';
+import { screenRoutes } from '../constants';
 import { AuthStore } from '../stores';
+import { Theme } from '../theme/components';
 
 const styles = StyleSheet.create({
   container: {
@@ -22,11 +26,24 @@ class HomeScreen extends Component<HomeScreenProps> {
     title: 'Welcome to the app!',
   };
 
+  private onPressNewNote = () => {
+    this.props.navigation.navigate(screenRoutes.ProfileScreen);
+  };
+
   public render() {
     return (
       <View style={styles.container}>
         <Button title="Show me more of the app" onPress={this.showMoreApp} />
         <Button title="Actually, sign me out :)" onPress={this.signOutAsync} />
+        <Fab
+          direction="up"
+          containerStyle={{}}
+          style={{ backgroundColor: Theme.palette.primary }}
+          position="bottomRight"
+          onPress={this.onPressNewNote}
+        >
+          <Icon name="plus" type="Feather" />
+        </Fab>
       </View>
     );
   }
